1fd8751b3e0cbb3a5ee80e8c0100ffb405f5fd89
cirne <cirnemiro@gmail.com>
2021-01-09 14:01:27 +0100

    pause

--- assets/scripts/filter/filter.js
+++ assets/scripts/filter/filter.js
@@ -3,7 +3,7 @@ import { allInkers } from '../firebase.js'
  
 export const filterImg = ()=>{
     let allImages = []
-    console.log(allInkers);
+    
     allInkers.forEach(inker=>{
         inker.img.forEach(img=>{
             allImages.push({img,name:inker.profile.name})
--- assets/scripts/galery/modalGalery.js
+++ assets/scripts/galery/modalGalery.js
@@ -5,8 +5,22 @@ export const printDataModalGalery = (data)=>{
     const $navMobileHamburger = document.querySelector('.header_nav-mobile_bar__hamburger')
     const $navMobileArrow = document.querySelector('.header_nav-mobile_bar__arrow')
     const $modalGaleryPhoto = document.querySelector('.modalGalery__photo')
+    const $modalTitle = document.querySelector('.modalGalery_header__title')
+    const $modalTags = document.querySelector('.modalGalery__tags')
+    const tags = data.tags.split(',')
+
+    console.log(data);
 
     $modalGaleryPhoto.style.backgroundImage=`url('${data.url}')`
+    $modalTitle.innerHTML=data.name
+
+    $modalTags.innerHTML=''
+    tags.forEach(tag => {
+        const p = document.createElement('p')
+        p.classList.add('modalGalery_tags_element')
+        p.textContent=`#${tag}`
+        $modalTags.appendChild(p)
+    });
 
     $navMobileHamburger.classList.add('hidden')
     $navMobileArrow.classList.remove('hidden')
@@ -19,23 +33,29 @@ export const printDataModalGalery = (data)=>{
 
 }
 export const printDataModalGaleryGrid = (data,allInkers)=>{
-    console.log(data,allInkers);
+    
     let photosOfInker = {}
     allInkers.forEach((e,i,a) => {
         if (e.profile.name === data.name) {
             photosOfInker = a[i]
         }
     });
-    console.log(photosOfInker);
+   
     const $gridContainer = document.querySelector('.modalGalery_galery__grid')
     $gridContainer.innerHTML=''
     photosOfInker.img.forEach(e => {
+        console.log(e);
+        console.log(data.name);
         const $gridContainerElement = document.createElement('div')
-
-
         $gridContainerElement.classList.add('modalGalery_galery_grid__element')
         $gridContainerElement.style.backgroundImage=`url('${e.url}')`
+        $gridContainerElement.dataset.url = e.url
+        $gridContainerElement.dataset.tags = e.tags
+        $gridContainerElement.dataset.name = data.name
         $gridContainer.appendChild($gridContainerElement)
+        $gridContainerElement.addEventListener('click',()=>{
+            printDataModalGalery(data)
+        })
     });
 
 }
